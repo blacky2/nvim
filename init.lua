fe792a14ad3e7ed50cbb53c6c815f1ba8d7371ab
blacky2 <schwarz-philipp@t-online.de>
2023-09-03 13:30:27 +0200

    minor clean up

--- init.lua
+++ init.lua
@@ -1,14 +1,3 @@
--- info = debug.getinfo(1, 'S')
--- print(info.source)
-
--- print(os.getenv('PWD'))
--- local config_path = os.getenv('MYVIMRC')
--- print(config_path)
--- print(vim.env['MYVIMRC'])
--- local lua_dir = string.sub(info.source, 2, string.len(info.source) - 8) .. 'lua'
--- print(lua_dir)
--- vim.opt.runtimepath:append(lua_dir)
-
 require('options')
 require('keymaps')
 require('commands')
